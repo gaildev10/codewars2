39e4a15d9b572c36d52c409976a2822b99b7289a
Gail Giles <gailgiles@Gails-MacBook-Pro.local>
2022-05-31 22:02:37 -0400

    completed 017

---
+++ solution017.js
@@ -0,0 +1,7 @@
+//Get the mean of an array
+
+function getAverage(marks){
+    //TODO : calculate the downward rounded average of the marks array
+    let newArr = marks.reduce((acc,c )=> acc + c)
+    return Math.floor(newArr/marks.length)
+  }
