8a01926ca53f195816f3c7f2762bc11a6d8d71eb
GailDev10 <GailDev10@gmail.com>
2022-07-05 18:00:19 -0400

    completed 020

---
+++ solution020.js
@@ -0,0 +1,30 @@
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return a ** b
+}
+    
+function subt(a,b){
+    return  a - b
+}
+
+// const add = (a, b) => a + b;
+// const subt = (a, b) => a - b;
+// const divide = (a, b) => a / b;
+// const multiply = (a, b) => a * b;
+// const mod = (a, b) => a % b;
+// const exponent = (a, b) => a ** b;
