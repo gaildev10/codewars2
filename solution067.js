e9639ba8fdccdd6d73dc6c6f104766ab1578c9bf
GailDev10 <GailDev10@gmail.com>
2022-08-30 15:55:54 -0400

    completed 067

--- solution067.js
+++ solution067.js
@@ -1,3 +1,18 @@
-//Combine Strings
+//Split the bill
 
-const combineNames = (...names) => names.join(' ');
+function splitTheBill(x) {
+    let total = 0,
+        count = 0
+    
+    for( let i in x){
+      count++
+      total += x[i]
+    }
+    let average = total/count
+    for(let i in x){
+      x[i] = +(x[i] - average).toFixed(2)
+    }
+  return x
+
+
+}
