117a0daa6603087aa9e72e9aa078eecafaffb8f9
GailDev10 <GailDev10@gmail.com>
2022-09-21 09:38:23 -0400

    completed 104

---
+++ solution104.js
@@ -0,0 +1,9 @@
+//Kata Example Twist
+
+var websites = new Array(1000).fill('codewars')
+
+// let array = []
+// for (let i = 1; i<=1000; i++) {
+//     array.push('codewars')
+// }
+// console.log(array)
