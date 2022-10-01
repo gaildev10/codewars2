af9b609d805147c6c8bf71ec1a01727733737c31
GailDev10 <GailDev10@gmail.com>
2022-10-01 13:03:32 -0400

    completed 114

---
+++ solution114.js
@@ -0,0 +1,10 @@
+//Shifty closures
+
+var abeName = 'Abe';
+var greet_abe = function() {
+  return "Hello, " + abeName + '!';
+};
+var benName = 'Ben';
+var greet_ben = function() {
+  return "Hello, " + benName + '!';
+};
