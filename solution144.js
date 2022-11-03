d02488c10e6e090fee31ee7360277fb206c6b6c6
GailDev10 <GailDev10@gmail.com>
2022-11-02 20:08:45 -0400

    completed 144

---
+++ solution144.js
@@ -0,0 +1,5 @@
+//The Coupon Code
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)
+  }
