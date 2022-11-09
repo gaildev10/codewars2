09d7b6c621d7b48225b074bf39ef4825447a3311
GailDev10 <GailDev10@gmail.com>
2022-11-09 08:54:47 -0500

    completed 155

---
+++ solution155.js
@@ -0,0 +1,10 @@
+//Multiples of 3 & 5
+function solution(number){
+    let sum = 0
+    for(let i = 0; i<number; i++){
+      if(i % 3 === 0 || i % 5 === 0){
+        sum += i
+      }
+    }
+    return sum
+  }
