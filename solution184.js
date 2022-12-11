238d57130c9e4c68efc99a52fb58f6ae3b2c646f
GailDev10 <GailDev10@gmail.com>
2022-12-10 19:15:28 -0500

    corrected order of the 1st if statement

--- solution184.js
+++ solution184.js
@@ -1,7 +1,7 @@
 //Count of positives / sum of negatives
 countPositivesSumNegatives = (input) => {
     let result = [0, 0]
-    if(input.length == 0 || input == null){
+    if(input == null || input.length == 0 ){
       return []
     } 
     for(let i of input){
