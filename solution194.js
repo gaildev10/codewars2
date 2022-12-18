2d3ffab1d7aec149dbb3c0f2d0e4218772e158da
GailDev10 <GailDev10@gmail.com>
2022-12-18 16:19:23 -0500

    completed 194

---
+++ solution194.js
@@ -0,0 +1,9 @@
+//
+
+function mergeArrays(arr1, arr2) {
+    let combined = arr1.concat(arr2);
+    combined = new Set(combined);
+    combined = Array.from(combined);
+    return combined.sort(( a, b)=> a-b);
+  }
+  
