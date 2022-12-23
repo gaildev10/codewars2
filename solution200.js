5412208e5f88db0ea9159644d5442d804164e7a0
GailDev10 <GailDev10@gmail.com>
2022-12-22 19:15:13 -0500

    Completed 200

---
+++ solution200.js
@@ -0,0 +1,6 @@
+//Determine offspring sex based on genes XX and XY chromosomes
+
+function chromosomeCheck(sperm) {
+    if(sperm === 'XY') return "Congratulations! You're going to have a son."
+    return "Congratulations! You're going to have a daughter."
+  }
