c1dfec4250b20b58f02d687190ab6bf4628168a7
GailDev10 <GailDev10@gmail.com>
2023-01-25 10:51:04 -0500

    completed 234

---
+++ solution234.js
@@ -0,0 +1,11 @@
+//Hurry up, the walls are closing in!
+
+
+function canEscape(walls) {
+    for (let i=0; i< walls.length; i++){
+      if(Math.min(...walls[i]) - i < 2){
+        return false
+      }
+    }
+    return true;
+  }
